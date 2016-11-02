5b961aa62cc79c4c7125d9b9fcb6854701d47f5f
Barrington W Thompson <BarringtonThom@gmail.com>
2016-11-02 11:33:48 -0400

    update my BASH REP

--- debugtest1.sh
+++ debugtest1.sh
@@ -1,5 +1,13 @@
-#!/bin/bash -x
-echo "Hello ${LOGNAME}"
-echo "Today is $(date)"
-echo "Users currently on the machine, and their processes:"
-w 
+#!/bin/bash -xv
+# echo "Hello ${LOGNAME}"
+# echo "Today is $(date)"
+# echo "Users currently on the machine, and their processes:"
+# w
+# Script to print currently logged in users information, and current date & time.
+ls -al
+echo "Hello $USER"
+echo -e "Today is \c ";date
+echo -e "Number of user login : \c" ; who | wc -l
+echo "Calendar"
+cal
+exit 0
